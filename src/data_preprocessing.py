6413d15edaa25926e7f6e832b730da22b8f0cdf3
Denis Trautner <denistrautner@web.de>
2025-09-21 21:48:22 +0200

    Adapt data pre processing

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -2,7 +2,7 @@ from typing import Dict, Tuple
 from datasets import load_dataset, ClassLabel
 
 def load_conll2003():
-    return load_dataset("conll2003")
+    return load_dataset("conll2003", trust_remote_code=True)
 
 def build_label_maps(features) -> Tuple[Dict[int, str], Dict[str, int]]:
     ner_feature: ClassLabel = features["ner_tags"].feature
@@ -10,26 +10,29 @@ def build_label_maps(features) -> Tuple[Dict[int, str], Dict[str, int]]:
     label2id = {v: k for k, v in id2label.items()}
     return id2label, label2id
 
-def tokenize_and_align(examples, tokenizer, label_all_tokens=False, max_length=None):
+def tokenize_and_align(batch, tokenizer, label_all_tokens=False, max_length=256):
     tokenized = tokenizer(
-        examples["tokens"],
-        truncation=True,
+        batch["tokens"],
         is_split_into_words=True,
+        truncation=True,
+        padding="max_length",
         max_length=max_length,
     )
-    labels = []
-    for i, word_ids in enumerate(tokenized.word_ids(batch_index=None)):
-        word_to_label = examples["ner_tags"][i]
-        prev_word_id = None
+
+    new_labels = []
+    for i, labels in enumerate(batch["ner_tags"]):
+        word_ids = tokenized.word_ids(batch_index=i)
+        previous_word_idx = None
         label_ids = []
-        for word_id in word_ids:
-            if word_id is None:
-                label_ids.append(-100)
-            elif word_id != prev_word_id:
-                label_ids.append(word_to_label[word_id])
+        for word_idx in word_ids:
+            if word_idx is None:
+                label_ids.append(-100)  # special tokens
+            elif word_idx != previous_word_idx:
+                label_ids.append(labels[word_idx])
             else:
-                label_ids.append(word_to_label[word_id] if label_all_tokens else -100)
-            prev_word_id = word_id
-        labels.append(label_ids)
-    tokenized["labels"] = labels
+                label_ids.append(labels[word_idx] if label_all_tokens else -100)
+            previous_word_idx = word_idx
+        new_labels.append(label_ids)
+
+    tokenized["labels"] = new_labels
     return tokenized
